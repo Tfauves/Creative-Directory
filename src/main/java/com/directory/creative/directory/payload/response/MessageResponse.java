7d6ec4110fc820d484a508c0b4e76b7bd6b9bf4a
Tranquility Base <tfauvelle@gmail.com>
2022-04-05 13:09:22 -0400

    auth implemented and tested

---
+++ src/main/java/com/directory/creative/directory/payload/response/MessageResponse.java
@@ -0,0 +1,14 @@
+package com.directory.creative.directory.payload.response;
+
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
