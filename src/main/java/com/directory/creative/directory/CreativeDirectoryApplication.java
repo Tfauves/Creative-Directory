849b66d3f32ea2b657a4ef266843af5b8624f1c0
Tranquility Base <tfauvelle@gmail.com>
2022-04-05 11:29:15 -0400

    initial commit

---
+++ src/main/java/com/directory/creative/directory/CreativeDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.directory.creative.directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativeDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativeDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/directory/creative/directory/CreativeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.directory.creative.directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
