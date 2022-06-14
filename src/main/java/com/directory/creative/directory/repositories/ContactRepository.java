1678a37c40a34e8faa19254f6752fe15930cf8e6
Tranquility Base <tfauvelle@gmail.com>
2022-06-14 10:44:29 -0400

    work on contact repository

---
+++ src/main/java/com/directory/creative/directory/repositories/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.directory.creative.directory.repositories;
+
+import com.directory.creative.directory.models.contact.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    Optional<Contact> findByContact_id(Long id);
+}
